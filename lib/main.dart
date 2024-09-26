746369853acd83d1245d9768549787dc67b3fc0d
William Velandia <wvelandia@gmail.com>
2024-09-26 16:38:02 -0500

    Proyecto Componentes en Flutter - Inicial

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// Con la extension instalada de Awesome Flutter Snippets, se realiza la creación de una aplicación de Flutter digitando mateapp + Tab. 
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
